6a7b5dfeddbfe97fed53963fd0d3ac4570f688f6
skyfor1996 <noyal70@yandex.ru>
2018-12-03 13:27:48 +0200

    Add files via upload

---
+++ list_employee.sql
@@ -0,0 +1,35 @@
+/*
+SQLyog Enterprise - MySQL GUI v7.13 
+MySQL - 5.5.5-10.1.16-MariaDB : Database - list_employee
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`list_employee` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `list_employee`;
+
+/*Table structure for table `worker` */
+
+DROP TABLE IF EXISTS `worker`;
+
+CREATE TABLE `worker` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` char(50) NOT NULL,
+  `age` int(11) NOT NULL,
+  `salary` int(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8;
+
+/*Data for the table `worker` */
+
+insert  into `worker`(`id`,`name`,`age`,`salary`) values (36,'Mike',26,10000),(37,'Leo',40,500),(38,'Kate',19,567);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
